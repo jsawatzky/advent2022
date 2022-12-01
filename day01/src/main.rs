60abbc8da2949f1297aba0a29a8cbeccf76265d8
Jacob Sawatzky <jsawatzky@outlook.com>
2022-12-01 00:43:39 -0500

    Day One

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,45 @@
 use std::fs;
 
 fn part1(input: &str) {
-    println!("part1: {}", 0)
+    let input: Vec<&str> = input.split("\n").collect();
+    let mut max = 0;
+    let mut sum = 0;
+    for line in input {
+        match line {
+            "" => {
+                if sum > max {
+                    max = sum;
+                }
+                sum = 0;
+            }
+            val => {
+                let val: u32 = val.parse().expect("must be an int");
+                sum = sum + val;
+            }
+        }
+    }
+    println!("part1: {}", max)
 }
 
 fn part2(input: &str) {
-    println!("part2: {}", 0)
+    let input: Vec<&str> = input.split("\n").collect();
+    let mut sums = Vec::new();
+    let mut sum = 0;
+    for line in input {
+        match line {
+            "" => {
+                sums.push(sum);
+                sum = 0;
+            }
+            val => {
+                let val: u32 = val.parse().expect("must be an int");
+                sum = sum + val;
+            }
+        }
+    }
+    sums.sort();
+    sums.reverse();
+    println!("part2: {}", sums[0] + sums[1] + sums[2])
 }
 
 fn main() {
