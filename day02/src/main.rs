dc1c96f381ea72ac4dc2d69a2c2a4c2fab0518b2
Jacob Sawatzky <jsawatzky@outlook.com>
2022-12-02 16:31:43 -0500

    Day Two

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,11 +1,81 @@
 use std::fs;
 
 fn part1(input: &str) {
-    println!("part1: {}", 0)
+    let mut score = 0;
+    for line in input.lines() {
+        let game: Vec<&str> = line.split_whitespace().collect();
+
+        match game[1] {
+            "X" => { score = score + 1 }
+            "Y" => { score = score + 2 }
+            "Z" => { score = score + 3 }
+            _ => panic!("invalid move")
+        }
+
+        match game[0] {
+            "A" => { 
+                match game[1] {
+                    "X" => { score = score + 3 }
+                    "Y" => { score = score + 6 }
+                    "Z" => { }
+                    _ => panic!("invalid move")
+                }
+             }
+            "B" => { 
+                match game[1] {
+                    "X" => { }
+                    "Y" => { score = score + 3 }
+                    "Z" => { score = score + 6 }
+                    _ => panic!("invalid move")
+                }
+             }
+            "C" => { 
+                match game[1] {
+                    "X" => { score = score + 6 }
+                    "Y" => { }
+                    "Z" => { score = score + 3 }
+                    _ => panic!("invalid move")
+                }
+             }
+            _ => panic!("invalid move")
+        }
+    }
+    println!("part1: {}", score)
 }
 
 fn part2(input: &str) {
-    println!("part2: {}", 0)
+    let mut score = 0;
+    for line in input.lines() {
+        let game: Vec<&str> = line.split_whitespace().collect();
+        match game[0] {
+            "A" => { 
+                match game[1] {
+                    "X" => { score = score + 3 }
+                    "Y" => { score = score + 4 }
+                    "Z" => { score = score + 8 }
+                    _ => panic!("invalid move")
+                }
+             }
+            "B" => { 
+                match game[1] {
+                    "X" => { score = score + 1 }
+                    "Y" => { score = score + 5 }
+                    "Z" => { score = score + 9 }
+                    _ => panic!("invalid move")
+                }
+             }
+            "C" => { 
+                match game[1] {
+                    "X" => { score = score + 2 }
+                    "Y" => { score = score + 6 }
+                    "Z" => { score = score + 7 }
+                    _ => panic!("invalid move")
+                }
+             }
+            _ => panic!("invalid move")
+        }
+    }
+    println!("part2: {}", score)
 }
 
 fn main() {
