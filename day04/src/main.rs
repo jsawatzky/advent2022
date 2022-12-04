a28a9619c6db8bde41d94b913320b967afc5059a
Jacob Sawatzky <jsawatzky@outlook.com>
2022-12-04 15:12:22 -0500

    Day Four

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,11 +1,39 @@
 use std::fs;
 
 fn part1(input: &str) {
-    println!("part1: {}", 0)
+    let mut count = 0;
+    for line in input.lines() {
+        let (e1, e2) = line.split_once(",").unwrap();
+        let (e1r1, e1r2) = e1.split_once("-").unwrap();
+        let (e2r1, e2r2) = e2.split_once("-").unwrap();
+        let e1r1: u32 = e1r1.parse().expect("must be int");
+        let e1r2: u32 = e1r2.parse().expect("must be int");
+        let e2r1: u32 = e2r1.parse().expect("must be int");
+        let e2r2: u32 = e2r2.parse().expect("must be int");
+
+        if (e1r1 >= e2r1 && e1r2 <= e2r2) || (e2r1 >= e1r1 && e2r2 <= e1r2) {
+            count += 1
+        }
+    }
+    println!("part1: {}", count)
 }
 
 fn part2(input: &str) {
-    println!("part2: {}", 0)
+    let mut count = 0;
+    for line in input.lines() {
+        let (e1, e2) = line.split_once(",").unwrap();
+        let (e1r1, e1r2) = e1.split_once("-").unwrap();
+        let (e2r1, e2r2) = e2.split_once("-").unwrap();
+        let e1r1: u32 = e1r1.parse().expect("must be int");
+        let e1r2: u32 = e1r2.parse().expect("must be int");
+        let e2r1: u32 = e2r1.parse().expect("must be int");
+        let e2r2: u32 = e2r2.parse().expect("must be int");
+
+        if (e1r1 <= e2r2 && e1r2 >= e2r1) || (e2r1 <= e1r2 && e2r2 >= e1r1) {
+            count += 1
+        }
+    }
+    println!("part2: {}", count)
 }
 
 fn main() {
